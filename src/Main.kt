ccf704283024b7ad16d79e877f776c0b5cedb9ec
andressamartinss <andressa.martins38@etec.sp.gov.br>
2024-02-24 13:20:24 -0300

    aula1

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    //tipos numéricos inteiros
+    val num1: Byte = 127
+    val num2: Short = 32767
+    val num3: Int = 2_147_463_647
+    val num4: Long = 9_223_372_03_854_775_887 //long.MAX_VALUE
+    // tipos Númericos reais
+    val num5: Float = 3.14F
+    val num6: Double = 3.14
+    //tipo caractere
+    val char: Char = '?' //outros exemplos... '1', 'g', ' '
+    //tipo caractere
+    val boolean: Boolean = true // ou false
+    println(listOf(num1, num2, num3, num4, num5, num6, char, boolean))
+    println(2 is Int)
+    println(2147483648 is Long)
+    println(1.0 is Double)
+    // tudo é objeto
+    println(10.dec())
+
+}
